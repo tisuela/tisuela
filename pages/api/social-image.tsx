37aee8959603ffc43b43983cd2b37b3f5c6b8611
tisuela <ntisuela@uci.edu>
2026-04-21 23:45:49 -0700

    smol boi

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -12,7 +12,6 @@ import {
 } from 'notion-utils'
 
 import * as libConfig from '@/lib/config'
-import interSemiBoldFont from '@/lib/fonts/inter-semibold'
 import { mapImageUrl } from '@/lib/map-image-url'
 import { notion } from '@/lib/notion-api'
 import { type NotionPageInfo, type PageError } from '@/lib/types'
@@ -151,15 +150,7 @@ export default async function OGImage(
     </div>,
     {
       width: 1200,
-      height: 630,
-      fonts: [
-        {
-          name: 'Inter',
-          data: interSemiBoldFont,
-          style: 'normal',
-          weight: 700
-        }
-      ]
+      height: 630
     }
   )
 }
