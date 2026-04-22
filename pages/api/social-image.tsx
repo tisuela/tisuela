b5375c146f4cde8772032b7160a9934de1e19eab
tisuela <ntisuela@uci.edu>
2026-04-21 23:59:52 -0700

    cooler social image?

--- pages/api/social-image.tsx
+++ pages/api/social-image.tsx
@@ -63,111 +63,69 @@ export default async function OGImage(
   return new ImageResponse(
     <div
       style={{
-        position: 'relative',
         width: '100%',
         height: '100%',
         display: 'flex',
         flexDirection: 'column',
-        backgroundColor: '#1F2027',
+        background:
+          'linear-gradient(135deg, rgb(17, 24, 39) 0%, rgb(45, 52, 57) 45%, rgb(15, 23, 42) 100%)',
         alignItems: 'center',
         justifyContent: 'center',
-        color: 'black'
+        color: '#f8fafc'
       }}
     >
-      {pageInfo.image && (
-        <img
-          src={pageInfo.image}
-          style={{
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-            objectFit: 'cover'
-            // TODO: satori doesn't support background-size: cover and seems to
-            // have inconsistent support for filter + transform to get rid of the
-            // blurred edges. For now, we'll go without a blur filter on the
-            // background, but Satori is still very new, so hopefully we can re-add
-            // the blur soon.
-
-            // backgroundImage: pageInfo.image
-            //   ? `url(${pageInfo.image})`
-            //   : undefined,
-            // backgroundSize: '100% 100%'
-            // TODO: pageInfo.imageObjectPosition
-            // filter: 'blur(8px)'
-            // transform: 'scale(1.05)'
-          }}
-        />
-      )}
-
       <div
         style={{
-          position: 'relative',
           width: 900,
-          height: 465,
+          height: 510,
           display: 'flex',
           flexDirection: 'column',
-          border: '16px solid rgba(0,0,0,0.3)',
-          borderRadius: 8,
-          zIndex: '1'
+          alignItems: 'center',
+          justifyContent: 'center',
+          border: '1px solid rgba(255, 255, 255, 0.2)',
+          borderRadius: 28,
+          backgroundColor: 'rgba(15, 23, 42, 0.45)'
         }}
       >
         <div
           style={{
-            width: '100%',
-            height: '100%',
             display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-around',
-            backgroundColor: '#fff',
-            padding: 24,
             alignItems: 'center',
-            textAlign: 'center'
+            justifyContent: 'center',
+            width: 220,
+            height: 220,
+            borderRadius: 9999,
+            border: '6px solid rgba(255, 255, 255, 0.32)',
+            backgroundColor: 'rgba(59, 130, 246, 0.2)',
+            boxShadow: '0 18px 46px rgba(0, 0, 0, 0.35)'
           }}
         >
-          {pageInfo.detail && (
-            <div style={{ fontSize: 32, opacity: 0 }}>{pageInfo.detail}</div>
-          )}
-
-          <div
-            style={{
-              fontSize: 70,
-              fontWeight: 700,
-              fontFamily: 'Inter'
-            }}
-          >
-            {pageInfo.title}
-          </div>
-
-          {pageInfo.detail && (
-            <div style={{ fontSize: 32, opacity: 0.6 }}>{pageInfo.detail}</div>
-          )}
+          <div style={{ fontSize: 132, fontWeight: 800, lineHeight: 1 }}>T</div>
         </div>
-      </div>
 
-      {pageInfo.authorImage && (
         <div
           style={{
-            position: 'absolute',
-            top: 47,
-            left: 104,
-            height: 128,
-            width: 128,
+            marginTop: 44,
             display: 'flex',
-            borderRadius: '50%',
-            border: '4px solid #fff',
-            zIndex: '5'
+            fontSize: 64,
+            fontWeight: 700,
+            letterSpacing: -1
           }}
         >
-          <img
-            src={pageInfo.authorImage}
-            style={{
-              width: '100%',
-              height: '100%'
-              // transform: 'scale(1.04)'
-            }}
-          />
+          {pageInfo.title || libConfig.name}
         </div>
-      )}
+
+        <div
+          style={{
+            marginTop: 16,
+            display: 'flex',
+            fontSize: 30,
+            opacity: 0.82
+          }}
+        >
+          {pageInfo.detail || libConfig.domain}
+        </div>
+      </div>
     </div>,
     {
       width: 1200,
